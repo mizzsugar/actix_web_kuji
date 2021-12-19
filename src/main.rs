aa5bb5b01fc34042185da8ea27af764ec7dfcec5
Mizuki Sato <mizki.sato.0025@gmail.com>
2021-12-19 16:18:22 +0900

    くじAPIを作成

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,53 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use rand::seq::SliceRandom;
+use serde::Deserialize;
 
-#[get("/")]
-async fn hello() -> impl Responder {
+async fn hello() -> HttpResponse {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[derive(Deserialize)]
+struct Kuji {
+    names: Vec<String>,
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn kuji(request_body: web::Json<Kuji>) -> HttpResponse {
+    let mut rng = rand::thread_rng();
+    HttpResponse::Ok().body(request_body.names.choose(&mut rng).unwrap())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/hello", web::get().to(hello))
+            .route("/kuji", web::post().to(kuji))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, web, App};
+    use serde_json::json;
+
+    #[actix_rt::test]
+    async fn test_kuji() {
+        let mut app = test::init_service(App::new().route("/kuji", web::post().to(kuji))).await;
+        let request_body = json!({ "names": vec!["a", "b", "c"] });
+        let choices = vec![
+            web::Bytes::from("a"),
+            web::Bytes::from("b"),
+            web::Bytes::from("c"),
+        ];
+        let req = test::TestRequest::post()
+            .uri("/kuji")
+            .set_json(&request_body)
+            .to_request();
+        let resp = test::read_response(&mut app, req).await;
+        assert!(choices.contains(&resp));
+    }
 }
